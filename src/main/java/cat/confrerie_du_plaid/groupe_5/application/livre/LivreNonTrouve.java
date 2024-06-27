40204051f3344f2fcb57498b28c8f366163a4c24
ohmushi <omnes.theo@gmail.com>
2024-06-27 14:48:55 +0200

    add naive implem

---
+++ src/main/java/cat/confrerie_du_plaid/groupe_5/application/livre/LivreNonTrouve.java
@@ -0,0 +1,7 @@
+package cat.confrerie_du_plaid.groupe_5.application.livre;
+
+public class LivreNonTrouve extends Exception {
+    public LivreNonTrouve(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/cat/confrerie_du_plaid/groupe_5/domain/Publications.java
@@ -0,0 +1,5 @@
+package cat.confrerie_du_plaid.groupe_5.domain;
+
+public interface Publications {
+    void enregistrer(Publication publication);
+}
