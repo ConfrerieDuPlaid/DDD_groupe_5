e1aa173c233c78f900557600c4632bda5498e64b
Nathan-dev-dot <maerna1@gmail.com>
2024-06-27 14:41:11 +0200

    add test

--- src/test/java/cat/confrerie_du_plaid/groupe_5/application/livre/PublierUneLectureTest.java
+++ src/test/java/cat/confrerie_du_plaid/groupe_5/application/livre/PublierUneLectureTest.java
@@ -1,7 +1,24 @@
 package cat.confrerie_du_plaid.groupe_5.application.livre;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PublierUneLectureTest {
 
+    @org.junit.jupiter.api.Test
+    void publierUneLectureAvecUnCommentaireEtUneNote() {
+        PublierUneLecture publierUneLecture = new PublierUneLecture();
+        assertNotNull(publierUneLecture.publier("1", null, "commentaire", 5.0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void publierUneLectureAvecUnCommentaireMaisPasDeNote() {
+        PublierUneLecture publierUneLecture = new PublierUneLecture();
+        assertNotNull(publierUneLecture.publier("1", null, "commentaire", 5.0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void publierUneLectureSansCommentaireMaisAvecUneNote() {
+        PublierUneLecture publierUneLecture = new PublierUneLecture();
+        assertNotNull(publierUneLecture.publier("1", null, "commentaire", 5.0));
+    }
 }
