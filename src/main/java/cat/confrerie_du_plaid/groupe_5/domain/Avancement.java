e7a8c77058ff3c836355fda62a87435723059dd4
Sarah Schlegel <sschlegel@myges.fr>
2024-06-27 15:14:41 +0200

    correct page errors

--- src/main/java/cat/confrerie_du_plaid/groupe_5/domain/Avancement.java
+++ src/main/java/cat/confrerie_du_plaid/groupe_5/domain/Avancement.java
@@ -1,24 +1,37 @@
 package cat.confrerie_du_plaid.groupe_5.domain;
 
 public class Avancement {
-    private Integer page;
-    private Integer chapitre;
+    private Integer pagesLues;
+    private Integer pagesTotales;
 
-    private Avancement(Integer page, Integer chapitre) {
-        this.page = page;
-        this.chapitre = chapitre;
+    private Avancement(Integer page, Integer pagesTotales) {
+        this.pagesLues = page;
+        this.pagesTotales = pagesTotales;
     }
 
-    public static Avancement sauvegarder(Integer page, Integer chapitre) {
-        if (page == null && chapitre == null) {
-            throw new IllegalArgumentException("La page ou le chapitre doivent être renseignés");
+    public static Avancement sauvegarder(Integer pagesLues, Integer pagesTotales) {
+        if (pagesLues == null) {
+            throw new IllegalArgumentException("La page courante doit être renseignée");
         }
-        if (page < 0 ){
+        if (pagesLues < 0 ){
             throw new IllegalArgumentException("La page doit être positive");
         }
-        if (chapitre < 0) {
-            throw new IllegalArgumentException("Le chapitre doit être positif");
+        if (pagesTotales != null) {
+            if (pagesTotales < 0) {
+                throw new IllegalArgumentException("Le nombre de pages totales doit être positif");
+            }
+            if (pagesTotales < pagesLues) {
+                throw new IllegalArgumentException("Le nombre de pages totales doit être supérieur ou égal à la page courante");
+            }
         }
-        return new Avancement(page, chapitre);
+        return new Avancement(pagesLues, pagesTotales);
+    }
+
+    public Integer getPagesLues() {
+        return pagesLues;
+    }
+
+    public Integer getPagesTotales() {
+        return pagesTotales;
     }
 }
--- src/test/java/cat/confrerie_du_plaid/groupe_5/domain/AvancementTest.java
+++ src/test/java/cat/confrerie_du_plaid/groupe_5/domain/AvancementTest.java
@@ -1,4 +1,48 @@
 package cat.confrerie_du_plaid.groupe_5.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AvancementTest {
+    @Test()
+    public void avancementPagesLuesNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Avancement avancement = Avancement.sauvegarder(null, 100);
+        });
+    }
+
+    @Test()
+    public void avancementPagesLuesNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Avancement avancement = Avancement.sauvegarder(-1, 100);
+        });
+    }
+
+    @Test()
+    public void avancementPagesTotalesNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Avancement avancement = Avancement.sauvegarder(1, -100);
+        });
+    }
+
+    @Test()
+    public void avancementPagesTotalesInferieurPagesLues() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Avancement avancement = Avancement.sauvegarder(100, 99);
+        });
+    }
+
+    @Test()
+    public void avancementAvecPagesLuesEtPagesTotales() {
+        Avancement avancement = Avancement.sauvegarder(50, 100);
+        Assertions.assertEquals(50, avancement.getPagesLues());
+        Assertions.assertEquals(100, avancement.getPagesTotales());
+    }
+
+    @Test()
+    public void avancementAvecPagesLuesSansPagesTotales() {
+        Avancement avancement = Avancement.sauvegarder(50, null);
+        Assertions.assertEquals(50, avancement.getPagesLues());
+        Assertions.assertNull(avancement.getPagesTotales());
+    }
 }
