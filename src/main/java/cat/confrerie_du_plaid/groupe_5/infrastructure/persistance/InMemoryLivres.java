ad3c316f0d5b69312116d2fbe71991bcdf9fa24b
ohmushi <omnes.theo@gmail.com>
2024-06-27 14:56:01 +0200

    implem method reecupererLivre

--- src/main/java/cat/confrerie_du_plaid/groupe_5/infrastructure/persistance/InMemoryLivres.java
+++ src/main/java/cat/confrerie_du_plaid/groupe_5/infrastructure/persistance/InMemoryLivres.java
@@ -1,6 +1,13 @@
 package cat.confrerie_du_plaid.groupe_5.infrastructure.persistance;
 
+import cat.confrerie_du_plaid.groupe_5.domain.Livre;
 import cat.confrerie_du_plaid.groupe_5.domain.Livres;
 
+import java.util.Optional;
+
 public class InMemoryLivres implements Livres {
+    @Override
+    public Optional<Livre> recupererUnLivreParSonId(String livreId) {
+        return Optional.empty();
+    }
 }
