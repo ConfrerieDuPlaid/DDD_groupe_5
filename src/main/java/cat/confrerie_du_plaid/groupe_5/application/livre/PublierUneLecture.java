de09613097ec84a221cbeb335cbba42cb9ad3a28
ohmushi <omnes.theo@gmail.com>
2024-06-27 14:32:07 +0200

    wip use case

--- src/main/java/cat/confrerie_du_plaid/groupe_5/application/livre/PublierUneLecture.java
+++ src/main/java/cat/confrerie_du_plaid/groupe_5/application/livre/PublierUneLecture.java
@@ -1,4 +1,11 @@
 package cat.confrerie_du_plaid.groupe_5.application.livre;
 
+import cat.confrerie_du_plaid.groupe_5.domain.Publication;
+
+import java.time.LocalDate;
+
 public class PublierUneLecture {
+    public Publication publier(String livreId, LocalDate dateDePublication, String commentaire, Double evaluation) {
+        return new Publication();
+    }
 }
